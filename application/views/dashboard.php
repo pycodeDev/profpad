c2ec96b8235cfc8b009a37db877399eab9cfd7a9
pycodeDev <anangfriendshell@gmail.com>
2019-12-24 19:25:46 +0700

    front end for user

---
+++ application/views/dashboard.php
@@ -0,0 +1,393 @@
+<!-- Masthead -->
+  <header class="masthead bg-primary text-white text-center">
+    <div class="container d-flex align-items-center flex-column">
+
+      <!-- Masthead Avatar Image -->
+      <img class="masthead-avatar mb-5" src="<?= base_url() ?>assets/img/avataaars.svg" alt="">
+
+      <!-- Masthead Heading -->
+      <h1 class="masthead-heading text-uppercase mb-0">Start Bootstrap</h1>
+
+      <!-- Icon Divider -->
+      <div class="divider-custom divider-light">
+        <div class="divider-custom-line"></div>
+        <div class="divider-custom-icon">
+          <i class="fas fa-star"></i>
+        </div>
+        <div class="divider-custom-line"></div>
+      </div>
+
+      <!-- Masthead Subheading -->
+      <p class="masthead-subheading font-weight-light mb-0">Graphic Artist - Web Designer - Illustrator</p>
+
+    </div>
+  </header>
+
+ <!-- Sejarah -->
+  <section class="page-section bg-white text-black mb-0" id="sejarah">
+    <div class="container">
+
+      <!-- About Section Heading -->
+      <h2 class="page-section-heading text-center text-uppercase text-black">Sejarah</h2>
+
+      <!-- Icon Divider -->
+      <div class="divider-custom divider-black">
+        <div class="divider-custom-line"></div>
+        <div class="divider-custom-icon">
+          <i class="fas fa-star"></i>
+        </div>
+        <div class="divider-custom-line"></div>
+      </div>
+
+      <!-- About Section Content -->
+      <div class="row">
+        <div class="col-lg-4 ml-auto">
+          <p class="lead">Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p>
+        </div>
+        <div class="col-lg-4 mr-auto">
+          <p class="lead">You can create your own custom avatar for the masthead, change the icon in the dividers, and add your email address to the contact form to make it fully functional!</p>
+        </div>
+      </div>
+
+      <!-- About Section Button -->
+      <div class="text-center mt-4">
+        <a class="btn btn-xl btn-outline-light" href="https://startbootstrap.com/themes/freelancer/">
+          <i class="fas fa-download mr-2"></i>
+          Free Download!
+        </a>
+      </div>
+
+    </div>
+  </section>
+ <!-- end Sejarah -->
+
+<!-- Contact Section -->
+<section class="page-section pt-5 circle-1 bg-primary " id="info">
+    <!-- Portfolio Section Heading -->
+    <h2 class="page-section-heading text-center text-uppercase  text-white mb-0" >Info Wisata</h2>
+    <!-- Icon Divider -->
+    <div class="divider-custom">
+      <div class="divider-custom-line" style="background-color:#fff"></div>
+      <div class="divider-custom-icon">
+        <i style="color:#fff" class="fas fa-star"></i>
+      </div>
+      <div class="divider-custom-line" style="background-color:#fff"></div>
+    </div>
+    <div class="row">
+      <div class="container pb-5 position-relative">
+        <div id="demo" class="carousel slide position-relative" data-ride="carousel">
+          <!-- Indicators -->
+          <ul class="carousel-indicators">
+            <li data-target="#demo" data-slide-to="0" class=""></li>
+            <li data-target="#demo" data-slide-to="1" class="active"></li>
+            <li data-target="#demo" data-slide-to="2" class=""></li>
+          </ul>
+          
+          <!-- The slideshow -->
+          <div class="carousel-inner">
+            <div class="carousel-item active carousel-item-left">
+              <div class="d-flex">
+                <div class="col-lg-4 col-sm-12 col-12 d-flex flex-column ">
+                  <a href="#" class="introduce pb-3">
+                    <div class="flex-column-1 ">
+                      <p class="pr-2 pl-3 pt-4">What a Service!!!!!Antony is one of the top Classy designer in the world.Exceeded expectation and designed my website to my requirments.Will be usingfor ongoing work.</p>
+                    </div>
+                    <div class="flex-column-2 d-flex">
+                      <div class="pic p1"></div>
+                      <div class="information  flex-column pl-3 pt-2">
+                        <p>David Beckham</p>
+                        <p class="text-gray">Managing Director</p>
+                      </div>
+                    </div>
+                    <span></span>
+                  </a>
+                </div>
+                <div class="col-lg-4 col-sm-12 col-12 d-flex flex-column ">
+                  <a href="#" class="introduce pb-3">
+                    <div class="flex-column-1 ">
+                      <p class="pr-2 pl-3 pt-4">What a Service!!!!!Antony is one of the top Classy designer in the world.Exceeded expectation and designed my website to my requirments.Will be usingfor ongoing work.</p>
+                    </div>
+                    <div class="flex-column-2 d-flex">
+                      <div class="pic p2"></div>
+                      <div class="information  flex-column pl-3 pt-2">
+                        <p>David Luiz</p>
+                        <p class="text-gray">Ceo &amp; Founder</p>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+                <div class="col-lg-4 col-sm-12 col-12 d-flex flex-column">
+                  <a href="#" class="introduce pb-3">
+                    <div class="flex-column-1 ">
+                      <p class="pr-2 pl-3 pt-4">What a Service!!!!!Antony is one of the top Classy designer in the world.Exceeded expectation and designed my website to my requirments.Will be usingfor ongoing work.</p>
+                    </div>
+                    <div class="flex-column-2 d-flex">
+                      <div class="pic p3"></div>
+                      <div class="information  flex-column pl-3 pt-2">
+                        <p>Donna Karan</p>
+                        <p class="text-gray">Fashion Designer</p>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="carousel-item carousel-item-next carousel-item-left">
+              <div class="d-flex">
+                <div class="col-lg-4 col-sm-12 col-12 d-flex flex-column ">
+                  <a href="#" class="introduce pb-3">
+                    <div class="flex-column-1 ">
+                      <p class="pr-2 pl-3 pt-4">What a Service!!!!!Antony is one of the top Classy designer in the world.Exceeded expectation and designed my website to my requirments.Will be usingfor ongoing work.</p>
+                    </div>
+                    <div class="flex-column-2 d-flex">
+                      <div class="pic p1"></div>
+                      <div class="information  flex-column pl-3 pt-2">
+                        <p>David Luiz</p>
+                        <p class="text-gray">Ceo &amp; Founder</p>
+                      </div>
+                    </div>
+                    <span></span>
+                  </a>
+                </div>
+                <div class="col-lg-4 col-sm-12 col-12 d-flex flex-column ">
+                  <a href="#" class="introduce pb-3">
+                    <div class="flex-column-1 ">
+                      <p class="pr-2 pl-3 pt-4">What a Service!!!!!Antony is one of the top Classy designer in the world.Exceeded expectation and designed my website to my requirments.Will be usingfor ongoing work.</p>
+                    </div>
+                    <div class="flex-column-2 d-flex">
+                      <div class="pic p2"></div>
+                      <div class="information  flex-column pl-3 pt-2">
+                        <p>David Luiz</p>
+                        <p class="text-gray">Ceo &amp; Founder</p>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+                <div class="col-lg-4 col-sm-12 col-12 d-flex flex-column">
+                  <a href="#" class="introduce pb-3">
+                    <div class="flex-column-1 ">
+                      <p class="pr-2 pl-3 pt-4">What a Service!!!!!Antony is one of the top Classy designer in the world.Exceeded expectation and designed my website to my requirments.Will be usingfor ongoing work.</p>
+                    </div>
+                    <div class="flex-column-2 d-flex">
+                      <div class="pic p3"></div>
+                      <div class="information  flex-column pl-3 pt-2">
+                        <p>Donna Karan</p>
+                        <p class="text-gray">Fashion Designer</p>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+              </div>
+              
+            </div>
+            <div class="carousel-item ">
+              <div class="d-flex">
+                <div class="col-lg-4 col-sm-12 col-12 d-flex flex-column ">
+                  <a href="#" class="introduce pb-3">
+                    <div class="flex-column-1 ">
+                      <p class="pr-2 pl-3 pt-4">What a Service!!!!!Antony is one of the top Classy designer in the world.Exceeded expectation and designed my website to my requirments.Will be usingfor ongoing work.</p>
+                    </div>
+                    <div class="flex-column-2 d-flex">
+                      <div class="pic p1"></div>
+                      <div class="information  flex-column pl-3 pt-2">
+                        <p>Donna Karan</p>
+                        <p class="text-gray">Fashion Designer</p>
+                      </div>
+                    </div>
+                    <span></span>
+                  </a>
+                </div>
+                <div class="col-lg-4 col-sm-12 col-12 d-flex flex-column ">
+                  <a href="#" class="introduce pb-3">
+                    <div class="flex-column-1 ">
+                      <p class="pr-2 pl-3 pt-4">What a Service!!!!!Antony is one of the top Classy designer in the world.Exceeded expectation and designed my website to my requirments.Will be usingfor ongoing work.</p>
+                    </div>
+                    <div class="flex-column-2 d-flex">
+                      <div class="pic p2"></div>
+                      <div class="information  flex-column pl-3 pt-2">
+                        <p>David Luiz</p>
+                        <p class="text-gray">Ceo &amp; Founder</p>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+                <div class="col-lg-4 col-sm-12 col-12 d-flex flex-column">
+                  <a href="#" class="introduce pb-3">
+                    <div class="flex-column-1 ">
+                      <p class="pr-2 pl-3 pt-4">What a Service!!!!!Antony is one of the top Classy designer in the world.Exceeded expectation and designed my website to my requirments.Will be usingfor ongoing work.</p>
+                    </div>
+                    <div class="flex-column-2 d-flex">
+                      <div class="pic p3"></div>
+                      <div class="information  flex-column pl-3 pt-2">
+                        <p>Donna Karan</p>
+                        <p class="text-gray">Fashion Designer</p>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+          
+          <!-- Left and right controls -->
+          
+          <a class="carousel-control-prev" href="#demo" data-slide="prev" style="opacity: 1; left: -10%;">
+            <i class="fas fa-chevron-left" style="color:#5969f6; font-size:30px;"></i>
+          </a>
+          <a class="carousel-control-next" href="#demo" data-slide="next" style="opacity: 1; transform: rotate(-180deg); right: -10%;">
+            <i class="fas fa-chevron-left" style="color:#5969f6; font-size:30px;"></i>
+          </a>       
+        </div>
+      </div>
+    </div>
+</section>
+
+  <!-- Portfolio Section -->
+  <section class="page-section portfolio" id="galery">
+    <div class="container">
+
+      <!-- Portfolio Section Heading -->
+      <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Galery</h2>
+
+      <!-- Icon Divider -->
+      <div class="divider-custom">
+        <div class="divider-custom-line"></div>
+        <div class="divider-custom-icon">
+          <i class="fas fa-star"></i>
+        </div>
+        <div class="divider-custom-line"></div>
+      </div>
+
+      <!-- Portfolio Grid Items -->
+      <div class="row">
+
+        <!-- Portfolio Item 1 -->
+        <div class="col-md-6 col-lg-4 mb-3">
+          <div class="card" style="width: 23rem;">
+            <img class="card-img-top" src="<?= base_url() ?>assets/img/portfolio/cake.png" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+               <div class="card-footer">
+                <button type="button" class="btn btn-outline-primary btn-block" data-toggle="modal" data-target="#portfolioModal2"><i class="fas fa-align-justify"></i> Detail</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Portfolio Modal 2 -->
+  <div class="portfolio-modal modal fade" id="portfolioModal2" tabindex="-1" role="dialog" aria-labelledby="portfolioModal2Label" aria-hidden="true">
+    <div class="modal-dialog modal-xl" role="document">
+      <div class="modal-content">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">
+            <i class="fas fa-times"></i>
+          </span>
+        </button>
+        <div class="modal-body text-center">
+          <div class="container">
+            <div class="row justify-content-center">
+              <div class="col-lg-8">
+                <!-- Portfolio Modal - Title -->
+                <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0">Tasty Cake</h2>
+                <!-- Icon Divider -->
+                <div class="divider-custom">
+                  <div class="divider-custom-line"></div>
+                  <div class="divider-custom-icon">
+                    <i class="fas fa-star"></i>
+                  </div>
+                  <div class="divider-custom-line"></div>
+                </div>
+                <!-- Portfolio Modal - Image -->
+                <img class="img-fluid rounded mb-5" src="img/portfolio/cake.png" alt="">
+                <!-- Portfolio Modal - Text -->
+                <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
+                <button class="btn btn-primary" href="#" data-dismiss="modal">
+                  <i class="fas fa-times fa-fw"></i>
+                  Close Window
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+        <!-- Portfolio Item 2 -->
+        <div class="col-md-6 col-lg-4">
+          <div class="card" style="width: 23rem;">
+            <img class="card-img-top" src="<?= base_url() ?>assets/img/portfolio/cake.png" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+               <div class="card-footer">
+                <button type="button" class="btn btn-outline-primary btn-block"><i class="fas fa-align-justify"></i> Detail</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Portfolio Item 3 -->
+        <div class="col-md-6 col-lg-4">
+          <div class="card" style="width: 23rem;">
+            <img class="card-img-top" src="<?= base_url() ?>assets/img/portfolio/cake.png" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <div class="card-footer">
+                <button type="button" class="btn btn-outline-primary btn-block"><i class="fas fa-align-justify"></i> Detail</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Portfolio Item 4 -->
+        <div class="col-md-6 col-lg-4">
+          <div class="card" style="width: 23rem;">
+            <img class="card-img-top" src="<?= base_url() ?>assets/img/portfolio/cake.png" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+               <div class="card-footer">
+                <button type="button" class="btn btn-outline-primary btn-block"><i class="fas fa-align-justify"></i> Detail</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Portfolio Item 5 -->
+        <div class="col-md-6 col-lg-4">
+          <div class="card" style="width: 23rem;">
+            <img class="card-img-top" src="<?= base_url() ?>assets/img/portfolio/cake.png" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+               <div class="card-footer">
+                <button type="button" class="btn btn-outline-primary btn-block"><i class="fas fa-align-justify"></i> Detail</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Portfolio Item 6 -->
+        <div class="col-md-6 col-lg-4">
+         <div class="card" style="width: 23rem;">
+            <img class="card-img-top" src="<?= base_url() ?>assets/img/portfolio/cake.png" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+               <div class="card-footer">
+                <button type="button" class="btn btn-outline-primary btn-block"><i class="fas fa-align-justify"></i> Detail</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <!-- /.row -->
+
+    </div>
+  </section>
+
