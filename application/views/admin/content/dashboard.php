3ffe23453083d858c5cbc4be1e064f6834e5a0f7
pycodeDev <44345333+pycodeDev@users.noreply.github.com>
2020-05-17 00:56:57 +0700

    Update dashboard.php
    fix

--- application/views/admin/content/dashboard.php
+++ application/views/admin/content/dashboard.php
@@ -7,10 +7,10 @@
             <img src="<?=base_url()?>assets_admin/dist/img/camp.svg" width="10%">
             <h5 class="card-title mt-2">Selamat Datang <b><?=$this->session->userdata('nama')?></b></h5>
             <p class="card-text">Silahkan Lakukan Pengelolaan mengenai Wisata dan Galery yang terbaru untuk data wisata yang akan ditampilkan di sistem.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <!-- <a href="#" class="btn btn-primary">Go somewhere</a>
                 <button type="button" class="btn btn-danger toastrDefaultError">
                   Launch Error Toast
-                </button>
+                </button> -->
         </div>
     </div>
 </div>
